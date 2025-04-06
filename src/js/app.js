20cd7d79967d6907e8c1ac41c42b3411cd0192b3
Wizp <shadrawizp@gmail.com>
2025-04-06 16:56:08 +0300

    all done

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,25 @@
-export const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
+export const swordman = {
+  name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+};
 
 export default function orderByProps(obj, sort) {
-    const result1 = [];
-    const result2 = [];
-    let sortKey;
-    for (let key in obj) {
-      sort.forEach(item => {
+  const result1 = [];
+  const result2 = [];
+  const sortKey = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      sort.forEach((item) => {
         if (item === key) {
-          result1.push({key: key, value: obj[key]});
-          sortKey = key;
-        } 
-      })
-      if (key !== sortKey) {
-        result2.push({key: key, value: obj[key]});
+          result1.push({ key, value: obj[key] });
+          sortKey.key = key;
+        }
+      });
+      if (key !== sortKey.key) {
+        result2.push({ key, value: obj[key] });
       }
     }
-    
-    result2.sort((a, b) => a.key > b.key ? 1 : -1)
-    return result1.concat(result2);
   }
+
+  result2.sort((a, b) => (a.key > b.key ? 1 : -1));
+  return result1.concat(result2);
+}
